eb70cb9c18740ed8f66149c6ad852226400464be
Natalia Bazhina <nataliaagapova27@yandex.ru>
2024-09-11 11:42:36 +0300

    fix load_data

--- catalog/management/commands/load_data.py
+++ catalog/management/commands/load_data.py
@@ -1,39 +1,46 @@
-# class Command(BaseCommand):
-#
-#     @staticmethod
-#     def json_read_categories():
-#         # Здесь мы получаем данные из фикстурв с категориями
-#
-#     @staticmethod
-#     def json_read_products():
-#         # Здесь мы получаем данные из фикстурв с продуктами
-#
-#     def handle(self, *args, **options):
-#
-#         # Удалите все продукты
-# 				# Удалите все категории
-#
-# 				# Создайте списки для хранения объектов
-#         product_for_create = []
-#         category_for_create = []
-#
-# 				# Обходим все значения категорий из фиктсуры для получения информации об одном объекте
-#         for category in Command.json_read_categories():
-#             category_for_create.append(
-#                 Category(название_поля=значение_из_словаря, ..., название_поля=значение_из_словаря)
-#             )
-#
-# 				# Создаем объекты в базе с помощью метода bulk_create()
-#         Category.objects.bulk_create(category_for_create)
-#
-# 				# Обходим все значения продуктов из фиктсуры для получения информации об одном объекте
-#         for product in Command.json_read_products():
-#             product_for_create.append(
-#                 Product(название_поля=значение_из_словаря, ...,
-# 												# получаем категорию из базы данных для корректной связки объектов
-#                         поле_категории=Category.objects.get(pk=значение_из_словаря), ...,
-#                         название_поля=значение_из_словаря)
-#             )
-#
-# 				# Создаем объекты в базе с помощью метода bulk_create()
-#         Product.objects.bulk_create(product_for_create)
+import json
+from django.core.management import BaseCommand
+from catalog.models import Product, Category
+
+
+class Command(BaseCommand):
+
+    @staticmethod
+    def json_read_categories():
+        with open('catalog/fixture/catalog_data.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            categories = []
+            for item in data:
+                if item['model'] == 'catalog.category':
+                    categories.append(item)
+            return categories
+
+
+    @staticmethod
+    def json_read_products():
+        with open('catalog/fixture/catalog_data.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            products = []
+            for item in data:
+                if item['model'] == 'catalog.product':
+                    products.append(item)
+            return products
+
+    def handle(self, *args, **options):
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+
+        categories_for_create = []
+        for category in Command.json_read_categories():
+            category_data = category["fields"]
+            categories_for_create.append(Category(id=category["pk"], **category_data))
+        Category.objects.bulk_create(categories_for_create)
+
+        products_for_create = []
+        for product in Command.json_read_products():
+            product_data = product["fields"]
+            category = Category.objects.get(pk=product_data.pop("category"))
+            products_for_create.append(
+                Product(id=product["pk"], category=category, **product_data)
+            )
+        Product.objects.bulk_create(products_for_create)
