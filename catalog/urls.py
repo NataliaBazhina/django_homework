5b5a18dfef99e19d414fd180dab4caa87cea6008
Natalia Bazhina <nataliaagapova27@yandex.ru>
2024-09-19 23:16:13 +0300

    change tree

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,7 +4,7 @@ from catalog.views import home, contact, product_list,products_detail
 
 app_name = CatalogConfig.name
 urlpatterns = [
-    path('', product_list, name='products_list'),
+    path('', product_list, name='product_list'),
     path('products/<int:pk>/', products_detail, name='products_detail'),
     path("home", home, name="home"),
     path("contacts/", contact, name="contact"),
